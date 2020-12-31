5a7bb71cf55368e7696366f7bb0bf7b55a1be09c
Bruno Neville Ribeiro Santos <bruno.santos@qflash.com.br>
2020-12-31 20:46:50 -0300

    Fixed config application.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(   admin.js admin.scss
-                                                    site.js site.scss
+Rails.application.config.assets.precompile += %w(   admin.js admin.css
+                                                    site.js site.css
                                                     admin_devise.js admin_devise.scss )
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   
   root 'site/dashboard#index'
+
+  
+  get '/show',to: 'site/dashboard#show'
+  
   
   namespace :admin do    
     root "dashboard#index"
     resources :bookshelf
+    resources :admin_profile, only: [:edit, :update]
   end
  
   devise_for :users
