00ca9a46ee269ffe3927f0dae7c7a3d637de919f
Bruno Neville Ribeiro Santos <bruno.santos@qflash.com.br>
2020-12-30 22:56:15 -0300

    Fixed controller to bookshelf#new.

--- app/controllers/admin/bookshelf_controller.rb
+++ app/controllers/admin/bookshelf_controller.rb
@@ -1,13 +1,54 @@
 class Admin::BookshelfController < AdminController
   before_action :set_active
+  before_action :set_book, only: [:edit, :update, :destroy]
   
   def index
-  end
+    @books = Book.all
+  end  
 
   def new
+    @book = Book.new
   end
 
-  def edit
+  def create
+    @book = Book.new(set_params)
+    respond_to do |format|
+      if @book.save!(set_params)
+        format.html{redirect_to admin_bookshelf_index_path, notice: 'Livro adicionado com Sucesso.'}
+      else
+        format.html{render :new, notice: 'Erro ao adicionado Livro.'}
+      end
+    end
+  end
+
+  def edit;end
+
+  def update
+    respond_to do |format|
+      if @book.save!(set_params)
+        format.html{redirect_to admin_bookshelf_index_path, notice: 'Livro atualizado com Sucesso.'}
+      else
+        format.html{render :new, notice: 'Erro ao atualizar Livro.'}
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      if @book.destroy!
+        format.html{redirect_to admin_bookshelf_index_path, notice: 'Livro deletado com Sucesso.'}
+      else
+        format.html{render :new, notice: 'Erro ao deletar Livro.'}
+      end
+    end
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def set_params
+    params.require(:book).permit(:name, :author, :title, :description)
   end
 
   def set_active
