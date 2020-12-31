a22be68a6b84fe4e72b8f5bcc27226d16a6236b3
Bruno Neville Ribeiro Santos <bruno.santos@qflash.com.br>
2020-12-30 22:56:46 -0300

    Create model book.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_190626) do
+ActiveRecord::Schema.define(version: 2020_12_30_223911) do
 
   create_table "admin_profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "admin_id"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2020_12_29_190626) do
 
   create_table "authors", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
