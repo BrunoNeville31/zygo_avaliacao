e8b788d5f0e6358974d5b6f16c3f5b80034d8834
Bruno Neville Ribeiro Santos <bruno.santos@qflash.com.br>
2021-01-01 15:43:56 -0300

    Fixed

--- config/application.rb
+++ config/application.rb
@@ -19,16 +19,7 @@ Bundler.require(*Rails.groups)
 
 module Bookshelf
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-    config.assets.paths << Rails.root.join('lib', 'assets', 'javascripts')
-    config.assets.paths << Rails.root.join('lib', 'assets', 'stylesheets')
-    # Don't generate system test files.
+    config.load_defaults 5.2    
     config.generators.system_tests = nil
   end
 end
